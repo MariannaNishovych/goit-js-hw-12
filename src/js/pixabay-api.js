0e97a9d4ccc160a06bf1ea5a28862d962bd756b6
Marianna Nishovych <nisovicmarianna@gmail.com>
2024-10-02 10:51:22 +0300

    add fix

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -23,19 +23,3 @@ export async function fetchImages(query, page = 1) {
 
 
 
-// try {
-//     const response = await axios.get(BASE_URL, {
-//     params: {
-//         key: API_KEY,
-//         q: query,
-//         image_type: 'photo',
-//         orientation: 'horizontal',
-//         safesearch: true,
-//         page: page,
-//         per_page: 15,
-//     },
-//     });
-//     return response.data;
-// } catch (error) {
-//     throw new Error("Images not found");
-// }
